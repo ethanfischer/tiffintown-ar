572f99b159a16461da561b94c963d8f3300de802
ethanfischer <ethan.fischer@incontextsolutions.com>
2025-04-26 21:28:36 -0500

    ui looking better

--- Assets/MenuManager.cs
+++ Assets/MenuManager.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.Serialization;
 using UnityEngine.UI;
@@ -12,6 +13,14 @@ public class MenuManager : MonoBehaviour
     Button _shopButton;
     [SerializeField]
     Button _profileButton;
+
+
+    [SerializeField]
+    GameObject _homeMenu;
+    [SerializeField]
+    GameObject _shopMenu;
+    [SerializeField]
+    GameObject _profileMenu;
     
 
     void Start()
@@ -23,16 +32,28 @@ public class MenuManager : MonoBehaviour
 
     void GoHome()
     {
+        _homeMenu.SetActive(true);
+        _shopMenu.SetActive(false);
+        _profileMenu.SetActive(false);
+        _backButton.GetComponentInChildren<TMP_Text>().text = "Home";
         Debug.Log("Go home");
     }
     
     void GoShop()
     {
+        _homeMenu.SetActive(false);
+        _shopMenu.SetActive(true);
+        _profileMenu.SetActive(false);
+        _backButton.GetComponentInChildren<TMP_Text>().text = "Shop";
         Debug.Log("Go shop");
     }
     
     void GoProfile()
     {
+        _homeMenu.SetActive(false);
+        _shopMenu.SetActive(false);
+        _profileMenu.SetActive(true);
+        _backButton.GetComponentInChildren<TMP_Text>().text = "Profile";
         Debug.Log("Go profile");
     }
 }
